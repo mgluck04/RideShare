42dafde870977a5f5d152581f2741186e63dec1d
Niko Vidalakis <nvidalakis23@students.polytechnic.org>
2022-02-02 08:59:23 -0800

    Changed car class

--- RideRunner.java
+++ RideRunner.java
@@ -1,6 +1,6 @@
 public class RideRunner {
     public static void main(String[] args) {
-         Car car1 = new Car();
+         Car car1 = new Car(0, 0, 5, 1, 2);
          People people1 = new People();
     }
 }
